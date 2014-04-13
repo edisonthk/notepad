30ca008c21b805bec77cdf5f0c2da27840cd56bd
Edisonthk <edisonthk@gmail.com>
2014-04-13 14:32:12 +0900

    Initial Commit

---
+++ Notepad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Notepad
+//
+//  Created by Edisonthk on 4/13/14.
+//  Copyright (c) 2014 Edisonthk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
